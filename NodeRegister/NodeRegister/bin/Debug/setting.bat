c47285f217131c45fafdbc7317ff8b6670d9049c
101st <gaoxuezhan@gmail.com>
2020-12-14 13:03:51 +0800

    :art:They are back.

---
+++ NodeRegister/NodeRegister/bin/Debug/setting.bat
@@ -0,0 +1,3 @@
+npm config set cache "G:\MidwayCommandCenter\MidWayBattleField\WeaponWorkspace\ＭyRegister\NodeRegister\NodeRegister\bin\Debug\node_cache"
+REM npm install -g cnpm --registry=https://registry.npm.taobao.org
+pause
